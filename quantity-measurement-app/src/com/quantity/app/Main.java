7354ce811d19371fd2629380d96f4550cecfe696
bloodybelly <writetobloodybelly@gmail.com>
2026-05-06 10:19:27 +0530

    UC7: Addition with explicit target unit support

--- quantity-measurement-app/src/com/quantity/app/Main.java
+++ quantity-measurement-app/src/com/quantity/app/Main.java
@@ -8,25 +8,25 @@ public class Main {
     public static void main(String[] args) {
 
         // =========================
-        // UC6 TEST CASES
+        // UC7 TEST CASES
         // =========================
 
         Quantity q1 = new Quantity(1, Unit.FEET);
         Quantity q2 = new Quantity(12, Unit.INCH);
 
-        Quantity result1 = q1.add(q2);
-        System.out.println("1 ft + 12 in = " + result1);
+        Quantity result1 = q1.add(q2, Unit.YARD);
+        System.out.println("1 ft + 12 in = " + result1 + " (in yards)");
 
-        Quantity q3 = new Quantity(1, Unit.YARD);
-        Quantity q4 = new Quantity(1, Unit.FEET);
+        Quantity q3 = new Quantity(2, Unit.YARD);
+        Quantity q4 = new Quantity(3, Unit.FEET);
 
-        Quantity result2 = q3.add(q4);
-        System.out.println("1 yard + 1 ft = " + result2);
+        Quantity result2 = q3.add(q4, Unit.FEET);
+        System.out.println("2 yd + 3 ft = " + result2 + " (in feet)");
 
         Quantity q5 = new Quantity(100, Unit.CM);
         Quantity q6 = new Quantity(1, Unit.FEET);
 
-        Quantity result3 = q5.add(q6);
-        System.out.println("100 cm + 1 ft = " + result3);
+        Quantity result3 = q5.add(q6, Unit.INCH);
+        System.out.println("100 cm + 1 ft = " + result3 + " (in inches)");
     }
 }
