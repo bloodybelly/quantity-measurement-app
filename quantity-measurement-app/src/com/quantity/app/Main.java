d91f93464d5064ddf091c21550df4118bab8f4fd
bloodybelly <writetobloodybelly@gmail.com>
2026-05-06 10:12:53 +0530

    UC4: Added YARD and CM support with conversion to base unit

--- quantity-measurement-app/src/com/quantity/app/Main.java
+++ quantity-measurement-app/src/com/quantity/app/Main.java
@@ -8,16 +8,19 @@ public class Main {
     public static void main(String[] args) {
 
         try {
-            // Same unit comparison
-            Quantity q1 = new Quantity(5, Unit.FEET);
-            Quantity q2 = new Quantity(5, Unit.FEET);
 
-            // Cross unit comparison
-            Quantity q3 = new Quantity(1, Unit.FEET);
-            Quantity q4 = new Quantity(12, Unit.INCH);
+            Quantity q1 = new Quantity(1, Unit.FEET);
+            Quantity q2 = new Quantity(12, Unit.INCH);
 
-            System.out.println("5 ft == 5 ft: " + q1.equals(q2));
-            System.out.println("1 ft == 12 in: " + q3.equals(q4));
+            Quantity q3 = new Quantity(1, Unit.YARD);
+            Quantity q4 = new Quantity(3, Unit.FEET);
+
+            Quantity q5 = new Quantity(30.48, Unit.CM); // 30.48 cm = 1 ft
+            Quantity q6 = new Quantity(1, Unit.FEET);
+
+            System.out.println("1 ft == 12 in: " + q1.equals(q2));
+            System.out.println("1 yd == 3 ft: " + q3.equals(q4));
+            System.out.println("30.48 cm == 1 ft: " + q5.equals(q6));
 
         } catch (Exception e) {
             System.out.println("Error: " + e.getMessage());
